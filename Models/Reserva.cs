c859c1b4bf210986287a1ffabf85e84156cc8e6d
William Costa <87620160+WilliamCoast@users.noreply.github.com>
2024-07-31 14:45:07 -0300

    31/07
    Update

---
+++ Models/Reserva.cs
@@ -0,0 +1,67 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI*
+        
+                    throw new ArgumentException("Excedeu o limite.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI* 
+
+            int quantidade = Hospedes.Count;
+            return quantidade;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI* 
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            decimal desconto  = valor * 0.1m; 
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                Console.WriteLine($"Desconto por mais de 10 dias reservados.\nDesconto total de: {desconto}");
+                valor = DiasReservados * Suite.ValorDiaria - desconto;
+            }
+
+            return valor;
+        }
+    }
+}
